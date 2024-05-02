aab71909c93f660f212e8117de714d76a89028cc
Ignacio Pickering <ign.pickering@gmail.com>
2024-05-02 00:59:47 -0400

    fix/ipickering/bug with monitor (#55)
    * Fix bug with monitor
    
    * Fix bug with monitor

--- anitune/callbacks.py
+++ anitune/callbacks.py
@@ -14,20 +14,34 @@ class ModelCheckpointWithMetrics(ModelCheckpoint):
     Checkpoint a model and also save the callback metrics from the trainer
     """
 
+    def check_monitor_top_k(
+        self, trainer: Trainer, current: tp.Optional[Tensor] = None
+    ) -> bool:
+        should_update_and_save = super().check_monitor_top_k(trainer, current)
+        if should_update_and_save:
+            self._dump_metrics(trainer)
+        return should_update_and_save
+
     def _save_topk_checkpoint(
         self, trainer: Trainer, monitor_candidates: tp.Dict[str, Tensor]
     ) -> None:
         super()._save_topk_checkpoint(trainer, monitor_candidates)
+        # In this case check_monitor_top_k is not called, and the
+        # metrics should be dump every step
+        if self.monitor is not None:
+            return
+        self._dump_metrics(trainer)
+
+    def _dump_metrics(self, trainer: Trainer) -> None:
         #  names of metrics are (energies|...)_(train|valid)_(rmse|mae)[kcal|mol[|ang]]
+        candidates = trainer.callback_metrics
         if self.dirpath is not None:
             dirpath = Path(self.dirpath).resolve()
         else:
             dirpath = Path(trainer.default_root_dir).resolve()
 
-        metrics: tp.Dict[str, tp.Union[int, float]] = {
-            "epoch": monitor_candidates["epoch"].item()
-        }
-        for k, v in monitor_candidates.items():
+        metrics: tp.Dict[str, tp.Union[int, float]] = {"epoch": trainer.current_epoch}
+        for k, v in candidates.items():
             if "_rmse" in k or "_mae" in k:
                 metrics[k] = v.item()
         with open(dirpath / "metrics.pkl", mode="wb") as fb:
